fe229f372e22aac9587df72c91a25ad11e62f223
Francisco Malva <francisco.goncalves.malva@gmail.com>
2025-01-02 22:02:17 +0000

    Fixed some code smells.

---
+++ DoaWVATool/SoundEffect.cs
@@ -0,0 +1,120 @@
+﻿namespace DoaWVATool;
+
+internal class SoundEffect
+{
+    private SoundEffectHeader _header;
+    private byte[]? _data;
+
+
+
+    public static SoundEffect FromPackedWva(BinaryReader br)
+    {
+        var se = new SoundEffect
+        {
+            _header = br.ReadUnmanaged<SoundEffectHeader>()
+        };
+
+        se._data = br.ReadBytes(se._header.DataSize);
+
+        return se;
+    }
+
+
+    public static SoundEffect FromWav(BinaryReader br)
+    {
+        var se = new SoundEffect();
+
+        while (br.BaseStream.Position < br.BaseStream.Length)
+        {
+            switch (br.Read4Cc())
+            {
+                case "RIFF":
+                    br.ReadInt32(); //Consume file size
+                    break;
+                case "WAVE":
+                    //Ignore
+                    break;
+                case "fmt ":
+                    unsafe
+                    {
+                        var sz = br.ReadInt32();
+                        fixed (byte* ptr = se._header.WaveFormatDataBuffer)
+                        {
+                            _ = br.Read(new Span<byte>(ptr, sz));
+                        }
+                        break;
+                    }
+                case "data":
+                    se._header.DataSize = br.ReadInt32();
+                    se._data = br.ReadBytes(se._header.DataSize);
+                    break;
+            }
+
+        }
+            
+
+        return se;
+    }
+
+    public static SoundEffect FromWavFile(string wavFilePath)
+    {
+        using var br = new BinaryReader(File.OpenRead(wavFilePath));
+        return FromWav(br);
+    }
+        
+
+
+    public void WriteAsWav(BinaryWriter bw)
+    {
+
+        if (_data == null)
+        {
+            throw new ArgumentException("_data should not be null!");
+        }
+        var begin = bw.BaseStream.Position;
+        bw.Write4Cc("RIFF");
+        bw.Write(0);
+
+        bw.Write4Cc("WAVE");
+        bw.Write4Cc("fmt ");
+        bw.Write(28);
+        unsafe
+        {
+            fixed (byte* ptr = _header.WaveFormatDataBuffer)
+            {
+                bw.Write(new Span<byte>(ptr, 28));
+            }
+                
+        }
+           
+        bw.Write4Cc("data");
+        bw.Write(_data.Length);
+        bw.Write(_data);
+        var end = bw.BaseStream.Position;
+
+
+        var length = end - begin;
+        bw.BaseStream.Position = begin + 4;
+        bw.Write((int)(length - 8));
+        bw.BaseStream.Position = end;
+    }
+
+    public void WriteToWavFile(string path)
+    {
+        using var binaryWriter = new BinaryWriter(File.Create(path));
+        WriteAsWav(binaryWriter);
+    }
+
+    public long WriteAsPartOfWva(BinaryWriter bw)
+    {
+        if (_data == null)
+        {
+            throw new ArgumentException("_data should not be null!");
+        }
+        var offset = bw.BaseStream.Position;
+        bw.WriteUnmanaged(_header);
+        bw.Write(_data);
+
+        return offset;
+    }
+}
--- DoaWVATool/WvaFile.cs
+++ DoaWVATool/WvaFile.cs
@@ -55,7 +55,7 @@ namespace DoaWVATool
             {
                 bw.BaseStream.Seek(0, SeekOrigin.End);
 
-                var writtenOffset = SoundEffects[i]?.Write(bw) ?? 0;
+                var writtenOffset = SoundEffects[i]?.WriteAsPartOfWva(bw) ?? 0;
 
                 bw.BaseStream.Position = 4 + i * 4;
                 bw.Write((int)writtenOffset);
@@ -72,6 +72,13 @@ namespace DoaWVATool
             Pack(binaryWriter);
         }
 
+
+
+        private static readonly JsonSerializerOptions JsonSettings =
+            new(JsonSerializerOptions.Default)
+            {
+                WriteIndented = true
+            };
 
         public void UnpackToDirectory(string path)
         {
@@ -93,11 +100,8 @@ namespace DoaWVATool
                 Console.WriteLine(manifest[i] == null ? $"Unpacked sound effect from slot {i} into {fullPath}" : $"Slot {i}contained no sound effect, skipping...");
             }
 
-            File.WriteAllText($"{path}/{Name}.json", JsonSerializer.Serialize(manifest,
-                new JsonSerializerOptions(JsonSerializerOptions.Default)
-                {
-                    WriteIndented = true
-                }));
+            File.WriteAllText($"{path}/{Name}.json", JsonSerializer.Serialize(manifest, JsonSettings));
+
         }
 
         public static WvaFile FromFile(string path)
@@ -119,9 +123,9 @@ namespace DoaWVATool
 
             var wvaFile = new WvaFile();
 
-            var manifestDir = Path.GetFullPath(Path.GetDirectoryName(manifestPath));
+            var manifestDir = Path.GetFullPath(Path.GetDirectoryName(manifestPath) ?? throw new InvalidOperationException("Manifest path resolved to null!"));
 
-            var soundEffects = JsonSerializer.Deserialize<string?[]>(File.ReadAllText(manifestPath));
+            var soundEffects = JsonSerializer.Deserialize<string?[]>(File.ReadAllText(manifestPath)) ?? throw new InvalidOperationException("Manifest resolved to null!");
 
 
             foreach (var soundEffectPath in soundEffects)
@@ -148,116 +152,5 @@ namespace DoaWVATool
         [FieldOffset(0)] public int DataSize;
         [FieldOffset(4)] public WavFormatData WaveFormatData;
         [FieldOffset(4)] public unsafe fixed byte WaveFormatDataBuffer[28];
-    }
-
-    internal class SoundEffect
-    {
-        private SoundEffectHeader _header;
-        private byte[] _data;
-        public string Name;
-
-
-
-        public static SoundEffect FromPackedWva(BinaryReader br)
-        {
-            var se = new SoundEffect
-            {
-                _header = br.ReadUnmanaged<SoundEffectHeader>()
-            };
-
-            se._data = br.ReadBytes(se._header.DataSize);
-
-            return se;
-        }
-
-
-        public static SoundEffect FromWav(BinaryReader br)
-        {
-            var se = new SoundEffect();
-
-            while (br.BaseStream.Position < br.BaseStream.Length)
-            {
-                switch (br.Read4Cc())
-                {
-                    case "RIFF":
-                        br.ReadInt32(); //Consume file size
-                        break;
-                    case "WAVE":
-                        //Ignore
-                        break;
-                    case "fmt ":
-                        unsafe
-                        {
-                            var sz = br.ReadInt32();
-                            fixed (byte* ptr = se._header.WaveFormatDataBuffer)
-                            {
-                                _ = br.Read(new Span<byte>(ptr, sz));
-                            }
-                            break;
-                        }
-                    case "data":
-                        se._header.DataSize = br.ReadInt32();
-                        se._data = br.ReadBytes(se._header.DataSize);
-                        break;
-                }
-
-            }
-            
-
-            return se;
-        }
-
-        public static SoundEffect FromWavFile(string wavFilePath)
-        {
-            using var br = new BinaryReader(File.OpenRead(wavFilePath));
-            return FromWav(br);
-        }
-        
-
-
-        public void WriteAsWav(BinaryWriter bw)
-        {
-            var begin = bw.BaseStream.Position;
-            bw.Write4Cc("RIFF");
-            bw.Write(0);
-
-            bw.Write4Cc("WAVE");
-            bw.Write4Cc("fmt ");
-            bw.Write(28);
-            unsafe
-            {
-                fixed (byte* ptr = _header.WaveFormatDataBuffer)
-                {
-                    bw.Write(new Span<byte>(ptr, 28));
-                }
-                
-            }
-           
-            bw.Write4Cc("data");
-            bw.Write(_data.Length);
-            bw.Write(_data);
-            var end = bw.BaseStream.Position;
-
-
-            var length = end - begin;
-            bw.BaseStream.Position = begin + 4;
-            bw.Write((int)(length - 8));
-            bw.BaseStream.Position = end;
-        }
-
-        public void WriteToWavFile(string path)
-        {
-            using var binaryWriter = new BinaryWriter(File.Create(path));
-            WriteAsWav(binaryWriter);
-        }
-
-        public long Write(BinaryWriter bw)
-        {
-            var offset = bw.BaseStream.Position;
-            bw.WriteUnmanaged(_header);
-            bw.Write(_data);
-
-            return offset;
-        }
     }
 }
